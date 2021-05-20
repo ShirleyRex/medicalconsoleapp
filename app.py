6ab7ed405acf8024488a246205c8c6464ef0166a
Shirley Ugwa <shirley.abaegbu@gmail.com>
2021-05-20 01:29:11 +0100

    created a new file

---
+++ app.py
@@ -0,0 +1,25 @@
+name = input("Hello, What is your name? ")
+print ('hello ' + name)
+age = int(input('How old are you ' + name + '? '))
+blood_group = input("What is your blood group? ")
+genotype = input("What is your genotype? ")
+fever = input("Are you feeling feverish " + name)
+fatigue = input("Do you feel tired? ")
+headache = input("Is your head aching you? ")
+sleepy = input("Do you feel sleepy? ")
+nausea = input("Do you feel like throwing up? ")
+is_feeling_sleepy = true
+is_having fever = true
+is_feeling_fatigue = true
+is_having_headache = true
+is_feeling_nausea = true
+
+if is_feeling_sleepy and is_having fever and is_feeling_fatigue and is_having_headache and is_feeling_nausea
+    print("Congratulations " + name + " you're pregnant")
+
+#if age > 30:
+    #print ("You are too old to be HOT...")
+#elif 20 < age < 30:
+    #print ("Don't be proud, since you might be hot...")
+#else:
+    #print ('This is not for you')
