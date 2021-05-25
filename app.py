9c26cdaff34d0678c5bb5200c93d757bce6aa045
Shirley Abaegbu <shirley.abaegbu@gmail.com>
2021-05-25 16:48:55 +0100

    updated file

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
-name = input("Hello, What is your name? ")
-print ('hello ' + name)
+name = input("Hello there, What is your name? ")
+print ('hello ' + name + ', nice having you here.')
 age = int(input('How old are you ' + name + '? '))
-blood_group = input("What is your blood group? ")
-genotype = input("What is your genotype? ")
-fever = input("Are you feeling feverish " + name)
+print ("Interesting, I bet you look way younger than your age.")
+blood_group = input("Please input your blood group and genotype" + name)
+print ("Thank you so much. Please answer the following questions, so I can properly diagnose what's wrong with you. I promise, it will only take a minute.")
+press_one = input("Press 1 if you feel feverish " + name)
 fatigue = input("Do you feel tired? ")
 headache = input("Is your head aching you? ")
 sleepy = input("Do you feel sleepy? ")
 nausea = input("Do you feel like throwing up? ")
-is_feeling_sleepy = true
-is_having fever = true
-is_feeling_fatigue = true
-is_having_headache = true
-is_feeling_nausea = true
+#is_feeling_sleepy = true
+#is_having fever = true
+#is_feeling_fatigue = true
+#is_having_headache = true
+#is_feeling_nausea = true
 
-if is_feeling_sleepy and is_having fever and is_feeling_fatigue and is_having_headache and is_feeling_nausea
-    print("Congratulations " + name + " you're pregnant")
+#if is_feeling_sleepy and is_having fever and is_feeling_fatigue and is_having_headache and is_feeling_nausea
+    #print("Congratulations " + name + " you're pregnant")
 
 #if age > 30:
     #print ("You are too old to be HOT...")
