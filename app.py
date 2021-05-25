28bc18e9fed51a46a00b08ec6332ad85cb9066f6
Shirley Abaegbu <shirley.abaegbu@gmail.com>
2021-05-25 17:20:44 +0100

    Added more updates

--- app.py
+++ app.py
@@ -4,23 +4,8 @@ age = int(input('How old are you ' + name + '? '))
 print ("Interesting, I bet you look way younger than your age.")
 blood_group = input("Please input your blood group and genotype" + name)
 print ("Thank you so much. Please answer the following questions, so I can properly diagnose what's wrong with you. I promise, it will only take a minute.")
-press_one = input("Press 1 if you feel feverish " + name)
-fatigue = input("Do you feel tired? ")
-headache = input("Is your head aching you? ")
-sleepy = input("Do you feel sleepy? ")
-nausea = input("Do you feel like throwing up? ")
-#is_feeling_sleepy = true
-#is_having fever = true
-#is_feeling_fatigue = true
-#is_having_headache = true
-#is_feeling_nausea = true
-
-#if is_feeling_sleepy and is_having fever and is_feeling_fatigue and is_having_headache and is_feeling_nausea
-    #print("Congratulations " + name + " you're pregnant")
-
-#if age > 30:
-    #print ("You are too old to be HOT...")
-#elif 20 < age < 30:
-    #print ("Don't be proud, since you might be hot...")
-#else:
-    #print ('This is not for you')
+press_one = input("Press one if you're having headache, cough and sore throat")#covid or cold
+press_two = input("Press two if you feel nausea, tired and if you're vomitting")#malaria or pregnancy
+press_three = input("Press three if you're having headache, loss of appetite, stomach pain")#typhoid or ulcer
+press_four = input("Press four if you have rashes, sore throat or chills")#HIV or allergic reaction
+press_five = input("Press five to start all over from the beginning")
